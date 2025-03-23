3df87092583a0b5ff1737f600905f78230966144
M-Haider-Rauf <pfm251@gmail.com>
2025-03-23 17:25:45 +0500

    enlarged event

--- src/Components/EventList/index.jsx
+++ src/Components/EventList/index.jsx
@@ -1,15 +1,15 @@
 export function EventList(props) {
     return(
-        <div className="flex flex-col w-48">
+        <div className="flex flex-col w-56">
             <div className="flex w-full bg-green-800 p-2">
-                <p className="font-impact text-white uppercase">Events</p>
+                <p className="font-impact text-white uppercase text-2xl">Events</p>
             </div>
-            <ul className="flex flex-col bg-green-700 px-2 py-4 gap-y-2">
+            <ul className="flex flex-col bg-green-700 px-2 py-2 gap-y-3">
             {
                 props.data.map(value => (
                     <li className="flex flex-col">
-                        <p className="font-impact text-sm text-white uppercase underline">{value.name}</p>
-                        <p className="text-white text-xs">{value.location}</p>
+                        <p className="font-impact text-lg text-white uppercase underline">{value.name}</p>
+                        <p className="text-white text-sm">{value.location}</p>
                     </li>
                 ))
             }
